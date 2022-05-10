cf3436faf0ee6dabc51906cd42d255d24b4ce329
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2022-05-10 13:28:09 -0600

    Test

---
+++ github.com/saragoodhue/Test.git/Test/src/questions.java
@@ -0,0 +1,14 @@
+
+public class questions
+	{
+
+		public static void main(String[] args)
+			{
+				int a = -8/5;
+				int b = 11/2;
+				double c = 14/4;
+				System.out.println(a+b+c);
+
+			}
+
+	}
